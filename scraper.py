f397b96a95115258c2b63021425e830ee2ecd6ef
aaronjacklin <ajacklin@gmail.com>
2015-12-23 12:31:06 -0500

    Update scraper.py
    First attempt at adapting code for my needs.

--- scraper.py
+++ scraper.py
@@ -1,17 +1,41 @@
+# Forked from Joel Eastwood's Globe Headlines (https://classic.scraperwiki.com/scrapers/bbc_headlines_8/) on Dec. 23, 2015
+# Modified by Aaron Jacklin
+# Additional commenting added as I figure out how the code works.
+
+# Import the ScraperWiki library, which is used to crawl the web.
+# This library will be used to store data to the SQL database.
 import scraperwiki
+
+# Import the urllib2 module, which is used to open URLs (https://docs.python.org/2/library/urllib2.html).
+# (Library vs module: http://stackoverflow.com/questions/19198166/whats-the-difference-between-a-module-and-a-library-in-python)
 import urllib2
+
+# Import the urlparse module, which breaks up and remakes URLs (https://docs.python.org/2/library/urlparse.html)
 import urlparse
+
+# Import the re module, which deals with regular expressions (https://docs.python.org/2/library/re.html)
 import re
+
+# Import the datetime module, which is used to manipulate dates and times (https://docs.python.org/2/library/datetime.html)
 import datetime
 
-site = 'http://www.theglobeandmail.com/'
+site = 'http://www.thestar.com/#/section/latestnews/'
         
 html = urllib2.urlopen(site).read()
-# print html
 
-everyheadline = re.findall('<h3[^>]*?>[\s]*?<a name="lt_Headline.1".*?>.*?</h3>', html, re.DOTALL)
-print len(everyheadline)
+# To limit the number of headlines, find the "Top Headlines" section of the latest news page
+topheadlines = re.findall('"Top Headlines"(.*?)"more-headlines"', html, re.DOTALL) #(.*?)<ul class="more-headlines"', html, re.DOTALL)
 
+# Return all "non-overlapping matches" of the pattern in topheadlines as a list of strings assigned to everyheadline 
+everyheadline = re.findall('<a href="(.*?)" class="headline">(.*?)</a>', topheadlines[0], re.DOTALL)
+
+# The following code doesn't work as expected and I haven't figured out why yet.
+#for headlineCode in everyheadline:
+#	headlines = re.search('<a[^>]*?href="(?P<link>[^"]*?)".*>(?P<headline>.+?)?</a>', headlineCode, re.DOTALL)
+#	link = urlparse.urljoin(site, headlines.group('link'))
+#	print link
+
+# The following lines are what remain of the original code
 data = {}
 
 for headline in everyheadline:
