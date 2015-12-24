0ac70ecb2aca4de7191812463df5dcadc1617345
aaronjacklin <ajacklin@gmail.com>
2015-12-24 01:41:36 -0500

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -4,9 +4,10 @@
 
 # Import the ScraperWiki library, which is used to crawl the web.
 # This library will be used to store data to the SQL database.
-import scraperwiki
+# import scraperwiki
 
-# Import the urllib2 module, which is used to open URLs (https://docs.python.org/2/library/urllib2.html).
+# Import the urllib2 module, which is used to open URLs
+# (https://docs.python.org/2/library/urllib2.html).
 # (Library vs module: http://stackoverflow.com/questions/19198166/whats-the-difference-between-a-module-and-a-library-in-python)
 import urllib2
 
@@ -23,19 +24,18 @@ site = 'http://www.thestar.com/#/section/latestnews/'
         
 html = urllib2.urlopen(site).read()
 
-# To limit the number of headlines, find the "Top Headlines" section of the latest news page
+# To limit the number of headlines, find the "Top Headlines" section of the latest news page. N.B. This only applies to the Toronto Star website as it's currently coded.
 topheadlines = re.findall('"Top Headlines"(.*?)"more-headlines"', html, re.DOTALL) #(.*?)<ul class="more-headlines"', html, re.DOTALL)
 
 # Return all "non-overlapping matches" of the pattern in topheadlines as a list of strings assigned to everyheadline 
-everyheadline = re.findall('<a href="(.*?)" class="headline">(.*?)</a>', topheadlines[0], re.DOTALL)
+everyheadline = re.findall('<a.*?href="(.*?)" class="headline">(.*?)</a>', topheadlines[0])
 
+# Create an empty dictionary called 'data'.
 data = {}
 
-# The following code doesn't work as expected and I haven't figured out why
-for headlineCode in everyheadline:
-	headlines = re.search('<a[^>]*?href="(?P<link>[^"]*?)".*>(?P<headline>.+?)</a>', headlineCode, re.DOTALL)
-	link = urlparse.urljoin(site, headlines.group('link'))
-	headline = headlines.group('headline').replace("&#039;", "'")
+for i in range(len(everyheadline)-1):
+	link = everyheadline[i][0]
+	headline = everyheadline[i][1]
 	data['headline'] = headline
 	data['URL'] = link
 	data['date'] = datetime.datetime.today().ctime()
